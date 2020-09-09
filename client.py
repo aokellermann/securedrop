0e25a050c06eb40d251b692438555207bf5b9da2
Antony Kellermann <aokellermann@gmail.com>
2020-09-09 13:04:30 -0400

    Implemented user registration

---
+++ client.py
@@ -0,0 +1,120 @@
+import json
+import getpass
+import os
+import hashlib
+import base64
+
+sd_filename = 'securedrop.json'
+
+
+def make_salt():
+    return os.urandom(32)
+
+
+class Authentication:
+    salt: bytes
+    key: bytes
+
+    def __init__(self, key=None, salt=None, jdict=None):
+        if key is None and jdict is None:
+            raise RuntimeError("Either a key or a jdict must be provided")
+
+        if salt is None:
+            salt = os.urandom(32)
+
+        if jdict is not None:
+            salt, key = base64.b64decode(jdict["salt"]), base64.b64decode(jdict["key"])
+        elif key is not None:
+            key = hashlib.pbkdf2_hmac('sha512', key.encode('utf-8'), salt, 10000)
+
+        self.salt, self.key = salt, key
+
+    def __eq__(self, other):
+        return self.salt == other.salt and self.key == other.key
+
+    def make_dict(self):
+        return {
+            "salt": base64.b64encode(self.salt).decode('ascii'),
+            "key": base64.b64encode(self.key).decode('ascii')
+        }
+
+
+class ClientData:
+    name: str
+    email: str
+    contacts: list
+    auth: Authentication
+
+    def __init__(self, nm=None, em=None, cs=None, password=None, jdict=None):
+        if jdict is not None:
+            self.name, self.email, self.contacts, self.auth = \
+                jdict["name"], jdict["email"], jdict["contacts"], Authentication(jdict=jdict["auth"])
+        else:
+            self.name, self.email, self.contacts, self.auth = nm, em, cs, Authentication(password)
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def make_dict(self):
+        return {
+            "name": self.name,
+            "email": self.email,
+            "contacts": self.contacts,
+            "auth": self.auth.make_dict()
+        }
+
+
+class RegisteredUsers:
+    users = dict()
+
+    def __init__(self, filename):
+        if os.path.exists(filename):
+            with open(filename, 'r') as f:
+                jdict = json.load(f)
+                for email, cd in jdict.items():
+                    self.users[email] = ClientData(jdict=cd)
+        else:
+            decision = input("No users are registered with this client.\nDo you want to register a new user (y/n)?")
+            if str(decision) == 'y':
+                self.register_new_user()
+            else:
+                raise RuntimeError("You must register a user before using securedrop")
+
+    def make_dict(self):
+        return {email: data.make_dict() for email, data in self.users.items()}
+
+    def write_json(self, filename):
+        with open(filename, 'w') as f:
+            json.dump(self.make_dict(), f)
+
+    def register_new_user(self):
+        name = input("Enter Full Name: ")
+        email = input("Enter Email Address: ")
+        if email in self.users:
+            raise RuntimeError("That email already exists!")
+
+        pw1 = getpass.getpass(prompt="Enter Password: ")
+        pw2 = getpass.getpass(prompt="Enter Password Again: ")
+        if name and email and pw1 and pw2:
+            salt = make_salt()
+            auth1 = Authentication(str(pw1), salt)
+            auth2 = Authentication(str(pw2), salt)
+            if auth1 != auth2:
+                raise RuntimeError("The two entered passwords don't match!")
+
+            self.users[email] = ClientData(name, email, [], pw1)
+        else:
+            raise RuntimeError("Invalid input")
+
+
+users = None
+
+
+def main():
+    global users
+    users = RegisteredUsers(sd_filename)
+    users.write_json(sd_filename)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/registration_test.py
@@ -0,0 +1,67 @@
+import unittest
+from unittest.mock import patch
+
+import client
+import os
+import json
+
+
+class InputSideEffect:
+    i = 0
+    lt: list
+
+    def __init__(self, lst):
+        self.lt = lst
+
+    def se(self, *args, **kwargs):
+        val = self.lt[self.i]
+        self.i += 1
+        return val
+
+
+class TestRegistration(unittest.TestCase):
+    def test_aaa_initial_ask_to_register_no_response_fails(self):
+        se = InputSideEffect(["n"])
+        with patch('builtins.input', side_effect=se.se):
+            with self.assertRaises(RuntimeError):
+                client.main()
+
+    def test_aab_initial_registration_succeeds(self):
+        se1 = InputSideEffect(["y", "name_v", "email_v"])
+        se2 = InputSideEffect(["password_v", "password_v"])
+        with patch('builtins.input', side_effect=se1.se):
+            with patch('getpass.getpass', side_effect=se2.se):
+                client.main()
+
+    def assert_initial_registered_users_dict_is_valid(self, d):
+        for email, cd in d.items():
+            self.assertEqual(email, "email_v")
+            self.assertEqual(cd["email"], email)
+            self.assertEqual(cd["name"], "name_v")
+            self.assertTrue(not cd["contacts"])
+            self.assertTrue(cd["auth"]["salt"])
+            self.assertTrue(cd["auth"]["key"])
+
+    def assert_initial_registered_users_is_valid(self, ru):
+        for email, cd in ru.items():
+            self.assertEqual(email, "email_v")
+            self.assertEqual(cd.email, email)
+            self.assertEqual(cd.name, "name_v")
+            self.assertTrue(not cd.contacts)
+            self.assertTrue(cd.auth.salt)
+            self.assertTrue(cd.auth.key)
+
+    def test_aac_initial_json_valid(self):
+        with open(client.sd_filename, 'r') as f:
+            jdict = json.load(f)
+            self.assert_initial_registered_users_dict_is_valid(jdict)
+
+    def test_aad_initial_load_from_json(self):
+        client.main()
+        self.assert_initial_registered_users_is_valid(client.users.users)
+
+
+if __name__ == '__main__':
+    if os.path.exists(client.sd_filename):
+        os.remove(client.sd_filename)
+    unittest.main()
