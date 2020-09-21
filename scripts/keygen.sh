109f49decf4d1082145615c840bdaf7a8ebc2824
Antony Kellermann <aokellermann@gmail.com>
2020-09-21 14:49:17 -0400

    Added generation of client cert/key

--- scripts/keygen.sh
+++ scripts/keygen.sh
@@ -1,8 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-openssl genrsa 2048 > server.key
-chmod 400 server.key
-openssl req -new -x509 -nodes -sha256 -days 365 -key server.key -out server.crt
+facets=('server' 'client')
 
-mv server.key securedrop
-mv server.crt securedrop
+for i in "${!facets[@]}";
+do
+  key_filename="${facets[$i]}.key"
+  cert_filename="${facets[$i]}.crt"
+
+  echo "${key_filename}"
+  echo "${cert_filename}"
+  openssl genrsa 2048 > "${key_filename}"
+  chmod 400 "${key_filename}"
+  openssl req -new -x509 -nodes -sha256 -days 365 -key "${key_filename}" -out "${cert_filename}"
+
+  mv "${key_filename}" securedrop/tests
+  mv "${cert_filename}" securedrop/tests
+done
---
+++ scripts/test.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+./scripts/keygen.sh < scripts/key_test_input.txt >/dev/null 2>/dev/null
+
+PYTHONPATH=$PYTHONPATH:. ./securedrop/tests/registration_test.py
+PYTHONPATH=$PYTHONPATH:. ./securedrop/tests/server_test.py
