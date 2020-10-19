120db5f0f59ddaa0c347101be0e9cde5e37f7cd5
Antony Kellermann <aokellermann@gmail.com>
2020-10-19 14:17:39 -0400

    Add CommandReceiver to __init__.py

--- securedrop/__init__.py
+++ securedrop/__init__.py
@@ -1,4 +1,5 @@
-__all__ = ['Client', 'Command']
+__all__ = ['Client', 'Command', 'CommandReceiver']
 
 from securedrop.client import Client
 from securedrop.command import Command
+from securedrop.command import CommandReceiver
