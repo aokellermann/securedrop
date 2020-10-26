2214a46b7ce2b4c21c11cc960a5e37b93c326c09
Antony Kellermann <aokellermann@gmail.com>
2020-10-26 18:11:51 -0400

    Fix test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,4 +3,4 @@
 ./scripts/keygen.sh < scripts/key_test_input.txt >/dev/null 2>/dev/null
 
 PYTHONPATH=$PYTHONPATH:. ./securedrop/tests/client_test.py
-PYTHONPATH=$PYTHONPATH:. ./securedrop/tests/server_test.py
+PYTHONPATH=$PYTHONPATH:. ./securedrop/tests/command_test.py
