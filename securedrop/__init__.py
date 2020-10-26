033e120d75041a5393418100e581b0ea6dc47111
Antony Kellermann <aokellermann@gmail.com>
2020-10-26 17:41:21 -0400

    Socket Comms (#6)
    ### Changes
    * Adds general Packet specification that should work for all the applications we need it for
    * Adds client/server facets for network communication
    * Uses IO multiplexing for concurrent communications
    * Adds unit tests, including fully working echo server
    
    Note: this branch includes work on #1, so that should be merged first.

--- securedrop/__init__.py
+++ securedrop/__init__.py
@@ -1,3 +1,5 @@
-__all__ = ['Client']
+__all__ = ['Client', 'Command', 'CommandReceiver']
 
 from securedrop.client import Client
+from securedrop.command import Command
+from securedrop.command import CommandReceiver
