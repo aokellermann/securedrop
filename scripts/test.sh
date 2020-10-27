5b46a05edb9d0f3bae3871cc9e9179b1cba93149
Antony Kellermann <aokellermann@gmail.com>
2020-10-27 12:31:02 -0400

    Add trap

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-set -e
-set -o pipefail
+set -eo pipefail
+
+trap "echo 'Tests failed!'" ERR
 
 rm -f securedrop/tests/*.json securedrop/tests/*.crt securedrop/tests/*.key
 bash scripts/keygen.sh < scripts/key_test_input.txt >/dev/null 2>/dev/null
 
-
 cd securedrop/tests
 PYTHONPATH=$PYTHONPATH:../.. python3 client_test.py
 PYTHONPATH=$PYTHONPATH:../.. python3 command_test.py
+
+echo "All tests passed successfully!"
