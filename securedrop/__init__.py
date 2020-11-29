ce92f36a6f18714b5afb327204b2fa52182131a5
jjjoycing123 <john.nay220@gmail.com>
2020-11-29 11:31:29 -0500

    Sanity check

--- securedrop/__init__.py
+++ securedrop/__init__.py
@@ -1,5 +1,8 @@
 __all__ = ['ClientBase', 'ServerBase', 'Client', 'Server', 'ServerDriver']
 
+print("Hello World")
+
+
 from securedrop.client_server_base import ClientBase, ServerBase
 from securedrop.client import Client
 from securedrop.server import Server, ServerDriver
