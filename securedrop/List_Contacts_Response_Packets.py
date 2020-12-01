17cbd6ce4978f173f6a1ea14f17ba09332149b75
jjjoycing123 <john.nay220@gmail.com>
2020-11-30 22:31:01 -0500

    Adjusted List_Contacts_Response_Packets.py

--- securedrop/List_Contacts_Response_Packets.py
+++ securedrop/List_Contacts_Response_Packets.py
@@ -1,19 +1,20 @@
 import json
 
-LIST_CONTACTS_RESPONSE_PACKETS_NAME = b"LCPRN"
+LIST_CONTACTS_RESPONSE_PACKETS_NAME = b"LCRN"
 
 
 class ListContactsPacketsResponse:
-    def __init__(self, contacts=None, data=None):
-        self.contacts = contacts
+    def __init__(self, message=None, data=None):
+        self.message = message
         self.jdict = dict()
         if data is not None:
             self.jdict = json.loads(data)
-            self.message = self.jdict["contacts"]
-        elif contacts is not None:
+            self.message = self.jdict["message"]
+        elif message is not None:
             self.jdict = {
-                "contacts": self.message,
+                "message": self.message,
             }
 
     def __bytes__(self):
         return LIST_CONTACTS_RESPONSE_PACKETS_NAME + bytes(json.dumps(self.jdict), encoding='ascii')
+
