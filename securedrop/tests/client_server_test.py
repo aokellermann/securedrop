fa9a02e66ba5da2b31224929ed8ddb4ed88b91e4
Antony Kellermann <aokellermann@gmail.com>
2020-12-06 08:59:40 -0500

    Add back test

--- securedrop/tests/client_server_test.py
+++ securedrop/tests/client_server_test.py
@@ -93,7 +93,17 @@ class TestRegistration(unittest.TestCase):
                         with patch('getpass.getpass', side_effect=se2.se):
                             client.main()
 
-    def test_aae_initial_registration_succeeds(self):
+    def test_aae_initial_ask_to_register_password_too_short(self):
+        """Ensures that client throws if the user inputs a password that is too short during registration."""
+        with server_process():
+            se1 = InputSideEffect(["y", "name_v", "email_v", "exit"])
+            se2 = InputSideEffect(["password_v1", "password_v1"])
+            with patch('builtins.input', side_effect=se1.se):
+                with patch('getpass.getpass', side_effect=se2.se):
+                    with self.assertRaises(RuntimeError):
+                        client.main()
+
+    def test_aaf_initial_registration_succeeds(self):
         """Ensures that client doesn't throw during some wild, yet valid user registrations."""
         valid_emails = ["simple@example.com", "very.common@example.com",
                         "x@example.com",
@@ -115,7 +125,7 @@ class TestRegistration(unittest.TestCase):
                         os.remove("client.json")
                         os.remove("server.json")
 
-    def test_aaf_initial_registration_succeeds(self):
+    def test_aag_initial_registration_succeeds(self):
         """Ensures that client doesn't throw during valid registration."""
         with server_process():
             se1 = InputSideEffect(["y", "name_v", "email_v@test.com", "exit"])
@@ -140,18 +150,18 @@ class TestRegistration(unittest.TestCase):
             self.assertTrue(cd.auth.salt)
             self.assertTrue(cd.auth.key)
 
-    def test_aag_initial_json_valid(self):
+    def test_aah_initial_json_valid(self):
         """Ensures that client serializes to JSON correctly after registration."""
         with open(DEFAULT_filename, 'r') as f:
             jdict = json.load(f)
             self.assert_initial_registered_users_dict_is_valid(jdict)
 
-    def test_aah_initial_load_from_json(self):
+    def test_aai_initial_load_from_json(self):
         """Ensures that client deserializes from JSON correctly."""
         serv = Server(DEFAULT_filename)
         self.assert_initial_registered_users_is_valid(serv.users.users)
 
-    def test_aai_login_unknown_email(self):
+    def test_aaj_login_unknown_email(self):
         """Ensures that client throws if trying to login with an unknown email."""
         with server_process():
             se1 = InputSideEffect(["email_v_@test.com"])
@@ -161,7 +171,7 @@ class TestRegistration(unittest.TestCase):
                     with self.assertRaises(RuntimeError):
                         client.main()
 
-    def test_aaj_login_wrong_password(self):
+    def test_aak_login_wrong_password(self):
         """Ensures that client throws if trying to login with an incorrect password."""
         with server_process():
             se1 = InputSideEffect(["email_v@test.com"])
@@ -171,7 +181,7 @@ class TestRegistration(unittest.TestCase):
                     with self.assertRaises(RuntimeError):
                         client.main()
 
-    def test_aak_login_correct_password(self):
+    def test_aal_login_correct_password(self):
         """Ensures that client logs in successfully with correct email/password."""
         with server_process():
             se1 = InputSideEffect(["email_v@test.com", "exit"])
@@ -180,7 +190,7 @@ class TestRegistration(unittest.TestCase):
                 with patch('getpass.getpass', side_effect=se2.se):
                     client.main()
 
-    def test_aal_add_contact_empty_input(self):
+    def test_aam_add_contact_empty_input(self):
         """Ensures that client does not add a new contact if the input is an empty string."""
         with server_process():
             for i in range(0, 2):
@@ -198,7 +208,7 @@ class TestRegistration(unittest.TestCase):
                                 jdict["e908de13f0f86b9c15f70d34cc1a5696280b3fbf822ae09343a779b19a3214b7"]["contacts"]))
                             self.assertEqual(dict(), contacts)
 
-    def test_aam_add_contact_invalid_email(self):
+    def test_aan_add_contact_invalid_email(self):
         """Ensures that client does not add a new contact if the input is an invalid email."""
         invald_emails = ["Abc.example.com", "A@b@c@example.com", "a\"b(c)d,e:f;g<h>i[j\\k]l@example.com",
                          "just\"not\"right@example.com", "this is\"not\\allowed@example.com",
@@ -221,7 +231,7 @@ class TestRegistration(unittest.TestCase):
                                 jdict["e908de13f0f86b9c15f70d34cc1a5696280b3fbf822ae09343a779b19a3214b7"]["contacts"]))
                             self.assertEqual(dict(), contacts)
 
-    def test_aan_add_contact(self):
+    def test_aao_add_contact(self):
         """Ensures that client adds valid contacts successfully."""
         with server_process():
             se1 = InputSideEffect(
@@ -239,7 +249,7 @@ class TestRegistration(unittest.TestCase):
                         self.assertEqual("name_v_2", contacts["email_v_2@test.com"])
                         self.assertEqual("name_v_3", contacts["email_v_3@test.com"])
 
-    def test_aao_login_correct_password_decrypt_contact(self):
+    def test_aap_login_correct_password_decrypt_contact(self):
         """Ensures that client logs in successfully with correct email/password Then decrypts contacts."""
         server = Server(DEFAULT_filename)
         user = server.users.users["e908de13f0f86b9c15f70d34cc1a5696280b3fbf822ae09343a779b19a3214b7"]
@@ -250,7 +260,7 @@ class TestRegistration(unittest.TestCase):
         self.assertEqual(user.contacts["email_v_2@test.com"], "name_v_2")
         self.assertEqual(user.contacts["email_v_3@test.com"], "name_v_3")
 
-    def test_aap_data_in_memory_after_decrypt(self):
+    def test_aaq_data_in_memory_after_decrypt(self):
         """Ensures that Client data can be accessed in local memory after decryption"""
         server = Server(DEFAULT_filename)
         user = server.users.users["e908de13f0f86b9c15f70d34cc1a5696280b3fbf822ae09343a779b19a3214b7"]
