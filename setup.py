5e4123b043ffbf3eff2912a138de86593dda1b30
Antony Kellermann <aokellermann@gmail.com>
2020-12-14 15:51:13 -0500

    Fix setup

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-from setuptools import setup
 from setuptools import find_packages
+from setuptools import setup
 
 setup(
     name='securedrop',
@@ -12,22 +12,19 @@ setup(
     description='Secure file sharing command line application.',
     python_requires='>=3.8',
     install_requires=[
-        'certifi>=2020.11.8',
+        'certifi>=2020.12.5',
         'chardet>=3.0.4',
-        'crypto>=1.4.1',
         'dnspython>=2.0.0',
         'email-validator>=1.1.2',
         'idna>=2.10',
         'Naked>=0.1.31',
-        'Padding>=0.5',
+        'nest>asyncio==1.4.3',
         'pycryptodome>=3.9.9',
         'PyYAML>=5.3.1',
         'requests>=2.25.0',
-        'securedrop>=1.0',
         'shellescape>=3.8.1',
         'tornado>=6.1',
         'urllib3>=1.26.2',
-        'yapf>=0.30.0'
     ],
 
 )
